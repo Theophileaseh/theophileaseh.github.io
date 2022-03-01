88be546ba08894ed345f7c3a1cc1c4820f0c17e0
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-01 13:27:18 +0300

    Add toggle effect to menu-list and hide menu when menu-item gets clicked

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,15 @@
 const toggleButton = document.querySelector('.mobile-button');
+const menu = document.querySelector('.menu-list');
+const menuItems = document.querySelectorAll('.menu-items');
 
 toggleButton.addEventListener('click', function() {
   toggleButton.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+menuItems.forEach(function (item) {
+  item.addEventListener('click', function() {
+    toggleButton.classList.remove('active');
+    menu.classList.remove('active');  
+  });
 });
