1c4d23df86f9a1d67dcc9720ebdcd553a71c28a3
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-01 12:11:52 +0300

    Add toggle menu icon from figma design

--- js/menu.js
+++ js/menu.js
@@ -1,2 +1,2 @@
-const toggleButton = document.querySelector('.mobile-button');
-toggleButton.appendChild('<i class="fa-solid fa-bars"></i>');
+// const toggleButton = document.querySelector('.mobile-button');
+// toggleButton.appendChild = '<i class="fa-solid fa-bars"></i>';
