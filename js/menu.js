894424546d8c942250d4caae173b1129fa6325fa
Theophileaseh <51954313+Theophileaseh@users.noreply.github.com>
2022-03-01 12:15:29 +0100

    Set menu display property

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,13 @@ const toggleButton = document.querySelector('.mobile-button');
 const menu = document.querySelector('.menu-list');
 const menuItems = document.querySelectorAll('.menu-items');
 
-toggleButton.addEventListener('click', function() {
+toggleButton.addEventListener('click', () => {
   toggleButton.classList.toggle('active');
   menu.classList.toggle('active');
 });
 
 menuItems.forEach(function (item) {
-  item.addEventListener('click', function() {
+  item.addEventListener('click', () => {
     toggleButton.classList.remove('active');
     menu.classList.remove('active');  
   });
