0c046458e4201ad06e53d0519a94c1d38200aa8c
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-01 16:06:23 +0300

    Fix scroll issue

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
 const toggleButton = document.querySelector('.mobile-button');
 const menu = document.querySelector('.menu-list');
 const menuItems = document.querySelectorAll('.menu-items');
+const pageBody = document.querySelector('body');
 
 toggleButton.addEventListener('click', () => {
   toggleButton.classList.toggle('active');
   menu.classList.toggle('active');
+  pageBody.classList.toggle('overflow-hidden');
 });
 
 menuItems.forEach((item) => {
@@ -13,3 +15,4 @@ menuItems.forEach((item) => {
     menu.classList.remove('active');
   });
 });
+
