549f839e01b8a29d866e48eb5994eeecc8d17b63
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-02 16:44:17 +0300

    Add projects.js file and add projects json data

---
+++ js/projects.js
@@ -0,0 +1,42 @@
+const projects = [
+  {
+    id: 1,
+    name: 'Multi-Post Stories',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. ",
+    featured_image: 'Rectangle_21.jpg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    live_link: '#',
+    source_link: '#',
+  },
+  {
+    id: 1,
+    name: 'Multi-Post Stories',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. ",
+    featured_image: 'Rectangle_21.jpg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    live_link: '#',
+    source_link: '#',
+  },
+  {
+    id: 1,
+    name: 'Multi-Post Stories',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. ",
+    featured_image: 'Rectangle_21.jpg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    live_link: '#',
+    source_link: '#',
+  },
+  {
+    id: 1,
+    name: 'Multi-Post Stories',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. ",
+    featured_image: 'Rectangle_21.jpg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    live_link: '#',
+    source_link: '#',
+  },
+];
