948c6bc41aa3e305cb828ee5d3294ac71f30db01
Theophileaseh <theophilenwachan@gmail.com>
2022-03-04 23:49:15 +0100

    Fix eslint errors

--- js/store.js
+++ js/store.js
@@ -1,4 +1,4 @@
-function store (){ // stores items in the localStorage
+function store() { // stores items in the localStorage
   const names = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
@@ -6,21 +6,20 @@ function store (){ // stores items in the localStorage
   const lastName = document.getElementById('last_name').value;
   const desktopEmail = document.getElementById('desktop_email').value;
   const message2 = document.getElementById('message2').value;
-  
   const submitted = {
     name: names,
-    email: email,
-    message: message,
+    email: email1,
+    message: message1,
     first_name: firstName,
     last_name: lastName,
     desktop_email: desktopEmail,
     message2: message2,
-  }
-  
-  window.localStorage.setItem(key,JSON.stringify(submitted));  
-      // converting object to string
+  };
+  let key = '';
+  window.localStorage.setItem(key,JSON.stringify(submitted));
+  // converting object to string
 }
-function retrieveRecords (){ // retrieves items in the localStorage
+function retrieveRecords () { // retrieves items in the localStorage
   const key = document.getElementById('retrieveKey').value; // gets key from user
   const records = window.localStorage.getItem(key); // searches for the key in localStorage
   const paragraph = document.createElement('p');
@@ -29,15 +28,14 @@ function retrieveRecords (){ // retrieves items in the localStorage
   const element = document.getElementById('retrieve');
   element.appendChild(paragraph);
 }
-  
-function removeItem (){ // deletes item from localStorage
+function removeItem() { // deletes item from localStorage
   const key = document.getElementById('removeKey').value; // gets key from user
   localStorage.removeItem(key); // passes key to the removeItem method
 }
-function clearStorage(){ // clears the entire localStorage
+function clearStorage (){ // clears the entire localStorage
   localStorage.clear();
 }
-window.onload = function (){ // ensures the page is loaded before functions are executed.
+window.onload = function bar() { // ensures the page is loaded before functions are executed.
   document.getElementById('carForm').onsubmit = store;
   document.getElementById('clearButton').onclick = clearStorage;
   document.getElementById('removeButton').onclick = removeItem;
