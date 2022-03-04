c786be1fccc394b0073b2425e416a1bf31e93b9b
Theophileaseh <theophilenwachan@gmail.com>
2022-03-04 20:41:47 +0100

    Fix eslint errors

--- js/validation.js
+++ js/validation.js
@@ -7,37 +7,35 @@ const errorMessage = document.querySelector('#error');
 
 console.log('form linked');
 
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+    
+  small.innerText = message;
+    
+  formControl.className = 'form-control error';
+  errorMessage.classList.add('smallerror');
+  form.classList.add('formerror');
+}
+
 function checkInputs() {
     //const firstnameValue = firstName.value.trim();
     //const lastnameValue = firstName.value.trim();
     //const nameValue = name.value.trim();
-    const emailValue = email.value.trim();
-    console.log(emailValue);
-    const lowerEmail = emailValue.toLowerCase();
+  const emailValue = email.value.trim();
+  const lowerEmail = emailValue.toLowerCase();
 
 
-    if (emailValue !== lowerEmail) {
-        setErrorFor(email, 'Email should be in lowercase');
-    return false;
-    } 
-    return true;
+  if (emailValue !== lowerEmail) {
+    setErrorFor(email, 'Email should be in lowercase');
+  return false;
+  } return true;
 }
 
 form.addEventListener('submit', (e) => {
-    if(checkInputs()) {
-    ;
+  if (checkInputs()) {
   } else {
     e.preventDefault();
   }
 });
 
-function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-  
-    small.innerText = message;
-  
-    formControl.className = 'form-control error';
-    errorMessage.classList.add('smallerror');
-    form.classList.add('formerror');
-  }
